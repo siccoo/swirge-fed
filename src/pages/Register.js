81b22f9081d68888fd29c6e51d7219116cf0d340
siccoo <obichilaka@gmail.com>
2021-03-01 12:17:42 +0100

    updates on Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,8 +7,36 @@ const Register = () => {
                 <h1>Register your account by filling your information below</h1>
                 <div className="form-inputs">
                     <label htmlFor="fullname" className="form-label">
-                        <input type="text" name="fullname" className="form-input" />
+                        Full Name
                     </label>
+                    <input 
+                        type="text" 
+                        name="fullname" 
+                        className="form-input" 
+                        placeholder="Enter your full name" 
+                    />
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="email" className="form-label">
+                        Email address
+                    </label>
+                    <input 
+                        type="email" 
+                        name="fullname" 
+                        className="form-input" 
+                        placeholder="Enter your email" 
+                    />
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="fullname" className="form-label">
+                        Password
+                    </label>
+                    <input 
+                        type="password" 
+                        name="fullname" 
+                        className="form-input" 
+                        placeholder="Enter your password" 
+                    />
                 </div>
             </form>
         </div>
