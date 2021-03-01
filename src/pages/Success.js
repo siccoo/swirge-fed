7539f5748398424b6191d290517354cb285c3afa
siccoo <obichilaka@gmail.com>
2021-03-01 15:32:08 +0100

    updates on validation.js & Success.js

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Form.css";
 
+import SuccessImg from "./../images/success-img.svg";
+
 const Success = () => {
     return (
         <div>
             <div className='form-content-right'>
                 <h1 className='form-success'>We have received your request!</h1>
-                <img className='form-img-2' src="../images/success-img.svg" alt='success-img' />
+                <img className='form-img-2' src={SuccessImg} alt='success-img' />
             </div>
         </div>
     )
--- src/validation.js
+++ src/validation.js
@@ -2,7 +2,7 @@ const validation = (values) => {
     let errors = {};
 
     if (!values.fullname.trim()) {
-        errors.fullname = 'First Name required';
+        errors.fullname = 'Full Name required';
     }
 
     if (!values.email) {
