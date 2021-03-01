04c41428e77776afe05a103f5de73208216baebd
siccoo <obichilaka@gmail.com>
2021-03-01 12:11:38 +0100

    updates on Register.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
 
+import Form from "./pages/Form";
+
 function App() {
   return (
     <div className="App">
-      
+      <Form />
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="form-content-right">
+            <form className="form">
+                <h1>Register your account by filling your information below</h1>
+                <div className="form-inputs">
+                    <label htmlFor="fullname" className="form-label">
+                        <input type="text" name="fullname" className="form-input" />
+                    </label>
+                </div>
+            </form>
         </div>
     )
 }
