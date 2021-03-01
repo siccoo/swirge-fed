24a21082de383190ab9072b57141c3537020a312
siccoo <obichilaka@gmail.com>
2021-03-01 19:42:18 +0100

    updates on EncodeProblem.js

--- EncodeProblem.js
+++ EncodeProblem.js
@@ -1,5 +1,14 @@
-function string(f) {
-    let queryString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+function ceasarCipher(text) {
+    let queryString = text.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", function(f) {
+        switch (f) {
+            case 'z': return 'a';
+            case 'c': return 'x';
+            case 'p': return 'k';
+            default:  return String.fromCharCode(1 + c.charCodeAt(0));
+        }
+    });
+    return caesar.replace(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]+/g, function(string) {
+        return string.toString();
+    });
 
-    
 }
