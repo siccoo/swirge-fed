1ce27e2bca52f22634b67da52fd4804203214a53
siccoo <obichilaka@gmail.com>
2021-03-01 14:31:44 +0100

    setting the isSubmit variables in Register.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Register from "./Register"
 
 const Form = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function submitForm() {
+        setIsSubmitted(true);
+    }
     return (
         <div>
             <Register />
